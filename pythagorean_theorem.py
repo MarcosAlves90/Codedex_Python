84b0bb789237fe32dd668a4b456963472e2ed62a
Marcos Alves <MarcosAlves25@proton.me>
2024-04-26 02:26:35 -0300

    Adicionar script Python para calcular o teorema de Pitágoras em pythagorean_theorem.py

---
+++ pythagorean_theorem.py
@@ -0,0 +1,9 @@
+# Write code below 💖
+
+import math
+
+a = int(input("Digite o valor do elemento a: "))
+print('')
+b = int(input("Digite o valor do elemento b: "))
+print('')
+print(math.sqrt((a**2)+(b**2)))
