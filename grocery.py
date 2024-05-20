f6ebfd6185f09a471ab79ff823402cc32b0e9cba
Marcos Alves <MarcosAlves25@proton.me>
2024-05-20 16:36:52 -0300

    feat: Adicionar scripts Python para exibir listas de compras e tarefas

---
+++ grocery.py
@@ -0,0 +1,10 @@
+# Write code below 💖
+
+# grocery.py
+
+grocery = ['🥚 Ovos','🥑 Abacates','🍪 Cookies','🌶️ Geleia de Pimenta','🫐 Mirtilos','🥦 Brócolis']
+
+print("📝 Lista de compras:\n")
+
+for item in grocery:
+  print(item)
